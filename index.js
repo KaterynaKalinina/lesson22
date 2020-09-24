2ff4943af715ba1b9d8a18dbcba19e53d240eea2
X <sersergiusx@gmail.com>
2020-09-24 21:21:31 +0300

    l

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const http = require("http");
+
 const { read, write } = require("./utils");
 
-const messages = read("messages");
+// const messages = read("messages");
 
-const newData = [
-    ...messages,
-    {
-        id: messages[messages.length - 1].id + 1,
-        name: "No-name",
-    },
-];
+// const newData = [
+//     ...messages,
+//     {
+//         id: messages[messages.length - 1].id + 1,
+//         name: "No-name",
+//     },
+// ];
 
-write("messages", newData);
+// write("messages", newData);
+
+const hostname = "localhost";
+const port = 3000;
+
+
+const server = http.createServer((request, response) => {
+    console.log(request);
+    response.end(JSON.stringify(read("messages")));
+});
+
+server.listen(port, hostname);
