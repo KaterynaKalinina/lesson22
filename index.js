12c7af004d1ba1d31abeaf3b60b85c6239c745bc
X <sersergiusx@gmail.com>
2020-10-19 19:07:29 +0300

    last

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
 const http = require("http");
-
+const url = require("url");
+const queryString = require("querystring");
 const { read, write } = require("./utils");
-
-// const messages = read("messages");
-
-// const newData = [
-//     ...messages,
-//     {
-//         id: messages[messages.length - 1].id + 1,
-//         name: "No-name",
-//     },
-// ];
-
-// write("messages", newData);
+const { filter, last, defaultTo, eq } = require("lodash");
 
 const hostname = "localhost";
 const port = 3000;
 
-
 const server = http.createServer((request, response) => {
-    console.log(request);
-    response.end(JSON.stringify(read("messages")));
+  const parsedUrl = url.parse(request.url);
+  const { id } = queryString.parse(parsedUrl.query);
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  const messages = read("messages");
+
+  switch (parsedUrl.pathname) {
+    case "/add":
+      const newData = [
+        ...messages,
+        {
+          id: messages[messages.length - 1].id + 1,
+          name: "No-name",
+        },
+      ];
+
+      write("messages", newData);
+      break;
+
+    case "/delete":
+    //   const newArray = [...messages];
+    //   if (id) {
+        
+    //     // const elementIndex = newArray.findIndex(el => el.id === Number(id));
+
+    //     // if (elementIndex !== -1) {
+    //     //   newArray.splice(elementIndex, 1);
+    //     // }
+    //   } else {
+    //     newArray.splice(newArray.length - 1, 1);
+    //   }
+
+      write(
+        "messages",
+        filter(
+            messages,
+            (message) => !eq(message.id, Number(defaultTo(id, last(messages).id)))
+        )
+    );
+  }
+
+  response.end(JSON.stringify(read("messages")));
 });
 
-server.listen(port, hostname);
+server.listen(port, hostname, () => {
+  console.log(`Server is listening ${hostname}:${port}`);
+})
